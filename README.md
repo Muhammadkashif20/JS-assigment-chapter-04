9b7cfbe2a8afdd48a0407acc23b1dbab7dd6d609
muhammadkashif20 <kashif28@gmail.com>
2024-03-02 00:04:14 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS-assigment-chapter-04
+ JS-assigment-chapter-04-Legal & illegal
---
+++ app.js
@@ -0,0 +1,28 @@
+//  Declare 3 variables in one statement.
+// Question 01 :- Legal:
+
+var dollar;
+var underscore;
+var text;
+var case_Sensetive;
+var camelCase;
+
+// Question 02 :- Illegal:
+
+var var;
+var kfdjdhuxuxshhs;
+var 1number;
+var &#%@^;
+var hsk sjh;
+
+// Question 03 :- Js Variables:
+
+document.write("Rules for naming JS variables ")
+document.write("<br>")
+document.write("Variable names can only contain, numbers, $  and _ . For Example : $my_1stVariable")
+document.write("<br>")
+document.write("Variables must begin with a letter, $ or_ .For example $name, _name or name")
+document.write("<br>")
+document.write("Variable names are case sensitive")
+document.write("<br>")
+document.write("Variable names should not be JS keywords")
